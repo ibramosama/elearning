c0fc0deebae77ad8d0563cf5439ba5a993bbbfec
mahmoudramadan0020 <mahmoudramadan0040@gmail.com>
2023-06-20 23:24:35 +0300

    intial create  react project

---
+++ Frontend/src/components/Login/Login.jsx
@@ -0,0 +1,8 @@
+import styleLogin from "./Login.module.css"
+function Login() {
+    return ( 
+        <div></div>
+    );
+}
+
+export default Login;
---
+++ Frontend/src/components/Register/Register.jsx
@@ -0,0 +1,8 @@
+import styleRegister from "./Register.module.css"
+function Register() {
+    return ( 
+        <div></div>
+    );
+}
+
+export default Register;
